515e148fb023e7137377b5854fd9199f51ee2b4e
rajeshb <bayyavarapur@virtusa.com>
2023-02-02 20:35:18 +0530

    Added throw again button

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-var randomNumber1 = Math.floor(Math.random()*6+1);
-var randomNumber2 = Math.floor(Math.random()*6+1);
+var temp1=0;
+var temp2=0;
+function rollAgain1(){
+    var randomNumber1 = Math.floor(Math.random()*6+1);
+    temp1 = randomNumber1;
+    document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
 
-document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
-console.log(randomNumber1)
-document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
-console.log(randomNumber1)
+}
+function rollAgain2(){
+    var randomNumber2 = Math.floor(Math.random()*6+1);
+    temp2 = randomNumber2;
+    document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
 
-if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerText = "player 1 wins";
 }
-else if(randomNumber1<randomNumber2){
-    document.querySelector("h1").innerText = "player 2 wins";
-}
-else{
-    document.querySelector("h1").innerText = "draw";
-}
+console.log(temp1);
+console.log(temp2);
+// if(randomNumber1>randomNumber2){
+//     document.querySelector("h1").innerText = "player 1 wins";
+// }
+// else if(randomNumber1<randomNumber2){
+//     document.querySelector("h1").innerText = "player 2 wins";
+// }
+// else{
+//     document.querySelector("h1").innerText = "draw";
+// }
