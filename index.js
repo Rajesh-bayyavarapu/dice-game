edacc953d9de227ef8a89436476deb0943c48a49
rajeshb <bayyavarapur@virtusa.com>
2023-02-03 18:12:37 +0530

    Updates game with player names

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-var temp1=0;
-var temp2=0;
-function rollAgain1(){
+var player1 = prompt("Enter player1 name");
+var player2 = prompt("Enter player2 name");
+
+document.querySelector("#player1").textContent = player1;
+document.querySelector("#player2").textContent = player2;
+
+function rollAgain(){
+    
     var randomNumber1 = Math.floor(Math.random()*6+1);
-    temp1 = randomNumber1;
     document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
-
-}
-function rollAgain2(){
     var randomNumber2 = Math.floor(Math.random()*6+1);
-    temp2 = randomNumber2;
     document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+    if(randomNumber1>randomNumber2){
+        document.querySelector("h1").innerText = player1+" wins";
+    }
+    else if(randomNumber1<randomNumber2){
+        document.querySelector("h1").innerText = player2 +" wins";
+    }
+    else{
+        document.querySelector("h1").innerText = "draw";
+    }
+    
 
 }
-console.log(temp1);
-console.log(temp2);
-// if(randomNumber1>randomNumber2){
-//     document.querySelector("h1").innerText = "player 1 wins";
-// }
-// else if(randomNumber1<randomNumber2){
-//     document.querySelector("h1").innerText = "player 2 wins";
-// }
-// else{
-//     document.querySelector("h1").innerText = "draw";
-// }
+
+
+
